2a9f27c46810cb14d25ddb3282c72de4303ee5bd
Alex Morega <alex@grep.ro>
2012-10-03 01:56:38 +0300

    raise KeyError on getitem

--- tests.py
+++ tests.py
@@ -6,8 +6,15 @@ class KV(object):
     def __len__(self):
         return 0
 
+    def __getitem__(self, key):
+        raise KeyError
+
 
 class KVTest(unittest.TestCase):
 
     def test_new_kv_is_empty(self):
         self.assertEqual(len(KV()), 0)
+
+    def test_get_missing_value_raises_key_error(self):
+        with self.assertRaises(KeyError):
+            KV()['missing']
