c2655d66eb5213ba82a974c83313d1960d77ad01
Alex Morega <alex@grep.ro>
2012-10-03 02:05:55 +0300

    store data in KV

--- tests.py
+++ tests.py
@@ -1,16 +1,30 @@
 import unittest
+import sqlite3
 
 
 class KV(object):
+
+    def __init__(self):
+        self._db = sqlite3.connect(':memory:')
+        self._execute('CREATE TABLE data (key TEXT PRIMARY KEY, value TEXT)')
+
+    def _execute(self, *args):
+        return self._db.cursor().execute(*args)
 
     def __len__(self):
         return 0
 
     def __getitem__(self, key):
-        raise KeyError
+        for row in self._execute('SELECT value FROM data WHERE key=?', (key,)):
+            return row[0]
+        else:
+            raise KeyError
 
     def get(self, key, fallback=None):
         return fallback
+
+    def __setitem__(self, key, value):
+        self._execute('INSERT INTO data VALUES (?, ?)', (key, value))
 
 
 class KVTest(unittest.TestCase):
@@ -28,3 +42,8 @@ class KVTest(unittest.TestCase):
     def test_get_missing_value_with_default_returns_argument(self):
         fallback = object()
         self.assertEqual(KV().get('missing', fallback), fallback)
+
+    def test_set_item_is_saved(self):
+        kv = KV()
+        kv['a'] = 'b'
+        self.assertEqual(kv['a'], 'b')
