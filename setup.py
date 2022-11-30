2332c07f909f835dcee766c65a557d0a98008599
Alex Morega <alex@grep.ro>
2022-11-30 14:28:09 +0200

    Version 0.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description=summary,
     long_description=readme_rst,
     license="BSD License",
-    version='0.3',
+    version='0.4',
     author='Alex Morega',
     py_modules=['kv'],
     classifiers=[
